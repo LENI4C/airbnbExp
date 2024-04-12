9f6796403c81a4d3aec54f6802e78e6780fe3362
LENI4C <timibraperetei74@gmail.com>
2024-04-12 19:39:52 +0100

    final commit

--- components/CarouselCard.jsx
+++ components/CarouselCard.jsx
@@ -1,36 +1,69 @@
 const CarouselCard = () => {
     return (
         <section className="carousel-section">
-            <Card />
-            <Card />
-            <Card />
+            <Card
+                userimage="/assets/carousel01.png"
+                rating="5.0 (6)"
+                country="USA"
+                userinfo="Life lessons from Katie Zaferes"
+                price="138"
+            />
+            <Card
+                userimage="/assets/carousel02.png"
+                rating="5.0 (30)"
+                country="USA"
+                userinfo="Learn wedding photography"
+                price="128"
+            />
+            <Card
+                userimage="/assets/carousel03.png"
+                rating="4.0 (2)"
+                country="USA"
+                userinfo="Group mountain biking"
+                price="80"
+            />
+            <Card
+                userimage="/assets/carousel03.png"
+                rating="4.0 (2)"
+                country="USA"
+                userinfo="Group mountain biking"
+                price="80"
+            />
+            <Card
+                userimage="/assets/carousel02.png"
+                rating="5.0 (30)"
+                country="USA"
+                userinfo="Learn wedding photography"
+                price="128"
+            />
+            <Card
+                userimage="/assets/carousel01.png"
+                rating="5.0 (6)"
+                country="USA"
+                userinfo="Life lessons from Katie Zaferes"
+                price="138"
+            />
         </section>
     );
 };
 
-export const Card = () => {
+export const Card = (props) => {
     return (
         <div className="card">
             <div className="card-image">
                 <img
                     className="user-image"
-                    src="/assets/carousel01.png"
-                    alt="carousel image 1"
+                    src={props.userimage}
+                    alt="carousel image"
                 />
             </div>
-            <CardInfo />
-        </div>
-    );
-};
-
-export const CardInfo = () => {
-    return (
-        <div className="card-text">
-            <div className="user-info">
-                <img src="/assets/star-icon.png" alt="star icon" />
-                <span> 5.0 (6) USA</span>
-                <p>Life lessons with Katie Zaferes</p>
-                <p> From $138 / person</p>
+            <div className="card-text">
+                <div className="user-info">
+                    <img src="/assets/star-icon.png" alt="star icon" />
+                    <span> {`${props.rating} ${props.country}`}</span>
+                    <p>{props.userinfo}</p>
+                    <p> From {`$${props.price}`} / person</p>
+                </div>
             </div>
         </div>
     );
