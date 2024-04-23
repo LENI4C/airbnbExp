11a8c6d0acea225a8459cce2c408b2b4de2a8a92
LENI4C <timibraperetei74@gmail.com>
2024-04-23 11:43:33 +0100

    props practice, beautiful stuff

---
+++ component data/carouselCard.js
@@ -0,0 +1,51 @@
+export default [
+    
+    {
+        userimage: "/assets/carousel01.png",
+        rating: "5.0 (6)",
+        country: "USA",
+        userinfo: "Life lessons from Katie Zaferes",
+        price: 138,
+    },
+
+    {
+        userimage: "/assets/carousel02.png",
+        rating: "5.0 (30)",
+        country: "USA",
+        userinfo: "Learn wedding photography",
+        price: 128,
+    },
+
+    {
+        userimage: "/assets/carousel03.png",
+        rating: "4.0 (2)",
+        country: "USA",
+        userinfo: "Group mountain biking",
+        price: 80,
+    },
+
+    {
+        userimage: "/assets/carousel03.png",
+        rating: "4.0 (2)",
+        country: "USA",
+        userinfo: "Group mountain biking",
+        price: 80,
+    },
+
+    {
+        userimage: "/assets/carousel02.png",
+        rating: "5.0 (30)",
+        country: "USA",
+        userinfo: "Learn wedding photography",
+        price: 128,
+    },
+
+    {
+        userimage: "/assets/carousel01.png",
+        rating: "5.0 (6)",
+        country: "USA",
+        userinfo: "Life lessons from Katie Zaferes",
+        price: 138,
+    }
+    
+];
--- components/CarouselCard.jsx
+++ components/CarouselCard.jsx
@@ -1,68 +1,39 @@
+import cardData from "../component data/carouselCard.js";
 const CarouselCard = () => {
+
+    const renderCard = cardData.map((datum) => (
+        <Card
+            userimage={datum.userimage}
+            rating={datum.rating}
+            country={datum.country}
+            userinfo={datum.userinfo}
+            price={datum.price}
+        />
+    ));
+
     return (
         <section className="carousel-section">
-            <Card
-                userimage="/assets/carousel01.png"
-                rating="5.0 (6)"
-                country="USA"
-                userinfo="Life lessons from Katie Zaferes"
-                price="138"
-            />
-            <Card
-                userimage="/assets/carousel02.png"
-                rating="5.0 (30)"
-                country="USA"
-                userinfo="Learn wedding photography"
-                price="128"
-            />
-            <Card
-                userimage="/assets/carousel03.png"
-                rating="4.0 (2)"
-                country="USA"
-                userinfo="Group mountain biking"
-                price="80"
-            />
-            <Card
-                userimage="/assets/carousel03.png"
-                rating="4.0 (2)"
-                country="USA"
-                userinfo="Group mountain biking"
-                price="80"
-            />
-            <Card
-                userimage="/assets/carousel02.png"
-                rating="5.0 (30)"
-                country="USA"
-                userinfo="Learn wedding photography"
-                price="128"
-            />
-            <Card
-                userimage="/assets/carousel01.png"
-                rating="5.0 (6)"
-                country="USA"
-                userinfo="Life lessons from Katie Zaferes"
-                price="138"
-            />
+            {renderCard}
         </section>
     );
 };
 
-export const Card = (props) => {
+export const Card = ({ userimage, rating, country, userinfo, price }) => {
     return (
         <div className="card">
             <div className="card-image">
                 <img
                     className="user-image"
-                    src={props.userimage}
+                    src={userimage}
                     alt="carousel image"
                 />
             </div>
             <div className="card-text">
                 <div className="user-info">
                     <img src="/assets/star-icon.png" alt="star icon" />
-                    <span> {`${props.rating} ${props.country}`}</span>
-                    <p>{props.userinfo}</p>
-                    <p> From {`$${props.price}`} / person</p>
+                    <span> {`${rating} ${country}`}</span>
+                    <p>{userinfo}</p>
+                    <p> From {`$${price}`} / person</p>
                 </div>
             </div>
         </div>
